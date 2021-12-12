a6c623f7377c212379f4c80bfce08b75dc6d7c18
Michael Tanner <m.tanner161@gmail.com>
2021-12-12 11:03:14 -0700

    Adding the model file with imports

---
+++ oilmodel.py
@@ -0,0 +1,8 @@
+## Top line code for our Oil Equity Model
+## We are using 4 test firms (ExxonMobil, Diamondback Resources, Devon Energy and ConocoPhillps)
+
+import statsmodels.api as sm
+import numpy as np
+import pandas as pd
+import os
+import json
